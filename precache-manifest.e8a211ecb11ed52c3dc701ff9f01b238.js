b54c9365850ad965473c276a3a1eb62d5ab4f32d
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-08-27 12:07:10 -0700

    Updates

---
+++ precache-manifest.e8a211ecb11ed52c3dc701ff9f01b238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287f29e9523f11dd0e420e23dc0bffa5",
+    "url": "/headtwist/index.html"
+  },
+  {
+    "revision": "b0ef607adcef6cbd3ba3",
+    "url": "/headtwist/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "44cd75d0ad0ba2056973",
+    "url": "/headtwist/static/css/main.9d23e564.chunk.css"
+  },
+  {
+    "revision": "b0ef607adcef6cbd3ba3",
+    "url": "/headtwist/static/js/2.22801fed.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/headtwist/static/js/2.22801fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cd75d0ad0ba2056973",
+    "url": "/headtwist/static/js/main.561a73c6.chunk.js"
+  },
+  {
+    "revision": "afb4430ce485759345d1",
+    "url": "/headtwist/static/js/runtime-main.561fe2b3.js"
+  }
+]);

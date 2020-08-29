74f9a81f7b14dbb3f4e7264f0590927bd92ee961
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-08-29 14:01:07 -0700

    Tied in api sign in

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,35 +1,83 @@
 import React from 'react';
 
-const Signin = ({onRouteChange}) => {
-  return (
-    <article className="mw6 center bg-clear br3 pa3 pa4-ns mv4 ba shadow-5 b--black-10">
-    <main className="pa4 black-80">
-    <div className="measure">
-      <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-        <legend className="f2 fw6 ph0 mh0">Log in</legend>
-        <div className="mt3">
-          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-          <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+class Signin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
+  }
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value});
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value});
+  }
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body:JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+    
+    .then(response => response.json())
+    .then(data => {
+      if (data === 'success') {
+        this.props.onRouteChange('home')
+      }
+    })
+  }
+
+  render() {
+    const { onRouteChange } = this.props
+    return (
+      <article className="mw6 center bg-clear br3 pa3 pa4-ns mv4 ba shadow-5 b--black-20">
+      <main className="pa4 black-80">
+      <div className="measure">
+        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+          <legend className="f1 fw6 ph0 mh0">Log in</legend>
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
+            <input 
+            className="pa1 input-reset ba bg-transparent hover-bg-transparent hover-transparent w-100 b--black-20" 
+            type="email" 
+            name="email"  
+            id="email" 
+            onChange={this.onEmailChange} 
+            />
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+            <input 
+            className="b pa1 input-reset ba bg-transparent hover-bg-transparent hover-white w-100 b--black-20" 
+            type="password" 
+            name="password"  
+            id="password" 
+            onChange={this.onPasswordChange} 
+            />
+          </div>
+          <label className="pa0 ma0 lh-copy f6 pointer"><input className="bg-transparent" type="checkbox" /> Remember me</label>
+        </fieldset>
+        <div className="">
+          <input 
+            onClick={this.onSubmitSignIn}
+            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Log in" />
         </div>
-        <div className="mv3">
-          <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-          <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+        <div className="lh-copy mt3">
+          <p onClick={() => onRouteChange('signup')} className="f6 link dim black db pointer">Not registered?</p>
         </div>
-        <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
-      </fieldset>
-      <div className="">
-        <input 
-          onClick={() => onRouteChange('home')}
-          className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Log in" />
       </div>
-      <div className="lh-copy mt3">
-        <p onClick={() => onRouteChange('signup')} className="f6 link dim black db pointer">Sign up</p>
-      </div>
-    </div>
-  </main>
-  </article>
-  
-  );
+    </main>
+    </article>
+    
+    );
+  }
 }
 
 export default Signin;
